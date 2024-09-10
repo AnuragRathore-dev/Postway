1a2bf37f606a07e4dbe138ba2cb6c16c5c77f392
AnuragRathore-dev <coderanucoder@gmail.com>
2024-09-10 16:16:32 +0530

    Update connection.js

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,23 +1,6 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-// METHOD 1 - Using IIFE's (Immediately Invoked Function Expression)
-
-// semi colon used for cleaning purpose
-
-// (async () => {
-//   try {
-//     const connectionInstance = await mongoose.connect(
-//       `${process.env.MONGODB_URL}/${DB_NAME}`
-//     );
-//   } catch (error) {
-//     console.log("ERROR: ", error);
-//     throw error;
-//   }
-// })();
-
-// METHOD 2
-
 export const dbConnect = async () => {
   try {
     const connectionInstance = await mongoose.connect(
