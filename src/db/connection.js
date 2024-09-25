c3d64b57776e3ca0ea098850928465f9689c10fc
AnuragRathore-dev <coderanucoder@gmail.com>
2024-09-25 21:50:06 +0530

    Update connection.js

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
-
 export const dbConnect = async () => {
   try {
     const connectionInstance = await mongoose.connect(
